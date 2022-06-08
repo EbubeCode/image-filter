ba4848b4511672906dbb00f1f9a23f75b7436ac1
EbubeCode <chukwuma258@gmail.com>
2022-06-08 14:17:17 +0100

    complete task server can now download, process and send image to client, and then delete the local downloaded file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,17 +30,36 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get("/filteredimage", async (req, res) => {
+    let { image_url } = req.query;
+    if (!image_url) {
+      return res.status(422).send('Image query cannot be null!');
+    }
+
+
+    console.log(image_url)
+    try {
+      let image_path = await filterImageFromURL(image_url);
+      res.status(200).sendFile(image_path);
+      res.on('finish', () => deleteLocalFiles([image_path]));
+    } catch (err) {
+      console.log(err);
+      return res.status(422).send('Image cannot be downloaded, check the url.');
+    }
+    
+
+  })
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import fs from "fs";
 import Jimp = require("jimp");
 
@@ -11,10 +12,17 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const photo =  await axios({
+        method: 'get',
+        url: inputURL,
+        responseType: 'arraybuffer'
+      })
+        .then(function ({ data: imageBuffer }) {
+          return Jimp.read(imageBuffer)
+        })
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-      await photo
+       photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
